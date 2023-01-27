66b020d4dfded71960b57a0a7fcabee82ddb8407
Mark J Menger <mjmenger@users.noreply.github.com>
2023-01-27 23:38:49 +0000

    remote azure site module

--- modules/azure-site/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "projectPrefix" {}
-variable "azureRegion" {}
-variable "resourceGroup" {}
-variable "hubVnetName" {}
-variable "volterraCloudCredAzure" {}
-variable "ssh_public_key" {}
-variable instanceSuffix {}
