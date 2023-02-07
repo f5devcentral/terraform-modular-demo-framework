298540a78273ec61146d51d322c805c431ab4113
Mark J Menger <mjmenger@users.noreply.github.com>
2023-02-07 22:17:25 +0000

    amend k8s config outputs

--- azure-cluster-discovery/main.tf
+++ azure-cluster-discovery/main.tf
@@ -61,6 +61,23 @@ output kubeconfig {
     value = azurerm_kubernetes_cluster.default.kube_config_raw
     sensitive = true
 }
+output k8s_host {
+  value     = azurerm_kubernetes_cluster.default.kube_config.0.host
+  sensitive = true
+}
+output k8s_client_certificate {
+  value     = azurerm_kubernetes_cluster.default.kube_config.0.client_certificate
+  sensitive = true
+}
+output k8s_client_key {
+  value     = azurerm_kubernetes_cluster.default.kube_config.0.client_key
+  sensitive = true
+}
+output k8s_cluster_ca_certificate {
+  value     = azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate
+  sensitive = true
+}
+
 resource local_file kubeconfig {
   filename = "${path.module}/kubeconfig.yaml"
   content  = azurerm_kubernetes_cluster.default.kube_config_raw
