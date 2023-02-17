75d1eb764891f19c963278b867e32064c6430727
Mark J Menger <mjmenger@users.noreply.github.com>
2023-02-17 22:49:32 +0000

    simple three node appstack site on azure

--- azure-appstack-site/azure-site.tf
+++ azure-appstack-site/azure-site.tf
@@ -4,8 +4,8 @@ resource "volterra_azure_vnet_site" "azure-site" {
   name           = format("%s-azureappstack-%s", var.projectPrefix, var.instanceSuffix)
   namespace      = "system"
   azure_region   = var.azureRegion
-  resource_group = "${var.resourceGroup}-site"
-  machine_type   = "Standard_D3_v2"
+  resource_group = "${var.resourceGroup}-appstack-site"
+  machine_type   = "Standard_D3_v2" # revisit this value to determine what is proper for AppStack
   ssh_key        = var.ssh_public_key
 
   #assisted                = false
@@ -24,79 +24,35 @@ resource "volterra_azure_vnet_site" "azure-site" {
     az_nodes {
       azure_az  = "1"
       disk_size = 80
-
-    #   inside_subnet {
-    #     subnet {
-    #       subnet_name         = "internal_subnet"
-    #       vnet_resource_group = true
-    #     }
-    #   }
-    #   outside_subnet {
-    #     subnet {
-    #       subnet_name         = "external_subnet"
-    #       vnet_resource_group = true
-    #     }
-    #   }
+      local_subnet {
+        subnet {
+          subnet_name         = "internal_subnet"
+          vnet_resource_group = true
+        }
+      }
     }
 
     az_nodes {
       azure_az  = "2"
       disk_size = 80
-
-    #   inside_subnet {
-    #     subnet {
-    #       subnet_name         = "internal_subnet"
-    #       vnet_resource_group = true
-    #     }
-    #   }
-    #   outside_subnet {
-    #     subnet {
-    #       subnet_name         = "external_subnet"
-    #       vnet_resource_group = true
-    #     }
-    #   }
+      local_subnet {
+        subnet {
+          subnet_name         = "internal_subnet"
+          vnet_resource_group = true
+        }
+      }
     }
 
     az_nodes {
       azure_az  = "3"
       disk_size = 80
-
-    #   inside_subnet {
-    #     subnet {
-    #       subnet_name         = "internal_subnet"
-    #       vnet_resource_group = true
-    #     }
-    #   }
-    #   outside_subnet {
-    #     subnet {
-    #       subnet_name         = "external_subnet"
-    #       vnet_resource_group = true
-    #     }
-    #   }
+      local_subnet {
+        subnet {
+          subnet_name         = "internal_subnet"
+          vnet_resource_group = true
+        }
+      }
     }
-    
-    # site_local_network {
-    #   new_network_autogenerate {
-    #     autogenerate = true
-    #   }
-    # }
-    # site_local_subnet {
-    #   new_subnet {
-    #     subnet_name  = format("%s-subnet-%02d", var.name, count.index)
-    #     primary_ipv4 = var.gcp_cidrs[count.index]
-    #   }
-    # }
-
-    # global_network_list {
-    #   global_network_connections {
-    #     slo_to_global_dr {
-    #       global_vn {
-    #         namespace = "system"
-    #         name      = volterra_virtual_network.global.name
-    #       }
-    #     }
-    #   }
-    # }
 
     no_network_policy        = true
     no_forward_proxy         = true
