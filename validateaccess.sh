e4d5200a6019720fe9c5c1d3f0463cfa372b5741
Mark J Menger <mjmenger@users.noreply.github.com>
2023-03-10 01:38:23 +0000

    doc: links to CLI install instructions

--- validateaccess.sh
+++ validateaccess.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-
 # CHECK FOR THE PLATFORM CLIs
 az_installed=1
 aws_installed=1
 gcloud_installed=1
-
 command -v az &> /dev/null ||  { 
-        echo 'az CLI is missing'; 
+        echo 'az CLI is missing. Instructions are at https://learn.microsoft.com/en-us/cli/azure/install-azure-cli'; 
         az_installed=0 
     }
 command -v aws &> /dev/null ||  {
-        echo 'aws CLI is missing'
+        echo 'aws CLI is missing. Instructions are at https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html'
         aws_installed=0
     }
 command -v gcloud &> /dev/null ||  {
-        echo 'gcloud CLI is missing'
+        echo 'gcloud CLI is missing. Instructions are at https://cloud.google.com/sdk/docs/install.'
         gcloud_installed=0
     }
-
 if [ $az_installed -eq 1 ] 
 then
     # CHECK FOR AZURE ACCESS
@@ -25,7 +22,6 @@ then
     az account show | jq .name
     echo TBD validate that the account is the expected one
     ## validate the the account name is appropriate
-
 fi
 # CHECK FOR AWS ACCESS
 if [ $aws_installed -eq 1 ] 
@@ -35,15 +31,12 @@ then
     aws sts get-caller-identity | jq .Account
     echo TBD validate that the account is the expected one
     ## validate the the account name is appropriate
-
 fi
-
 # CHECK FOR GOOGLE ACCESS
 if [ $gcloud_installed -eq 1 ] 
 then
     # CHECK FOR GOOGLE ACCESS
     echo Checking for Google account access
-    #az account show | jq .name
+    gcloud config list --format 'value(core.project)'
     ## validate the the account name is appropriate
-
 fi
