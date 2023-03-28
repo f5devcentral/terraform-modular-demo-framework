e8f22f54223ba0895c82b5386f39b3172253ecd5
Mark Menger <m.menger@f5.com>
2023-03-28 19:21:28 +0000

    remove temporary kubectl after install

--- .devcontainer/scripts/install-dev-tools.sh
+++ .devcontainer/scripts/install-dev-tools.sh
@@ -7,4 +7,4 @@ git clone https://github.com/cunymatthieu/tgenv.git ~/.tgenv \
     && echo 'export PATH="$HOME/.tgenv/bin:$PATH"' >> ~/.bashrc
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" \
-    && sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl    
+    && sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && rm kubectl
