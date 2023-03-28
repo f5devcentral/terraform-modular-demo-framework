edc1764ca74f4141e1828ac6636ea033a681b60c
Mark J Menger <mjmenger@users.noreply.github.com>
2023-03-28 17:49:12 +0000

    First attempt dev container

---
+++ .devcontainer/scripts/install-dev-tools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv \
+    && echo 'export PATH=$PATH:$HOME/.tfenv/bin' >> ~/.bashrc
+
+git clone https://github.com/cunymatthieu/tgenv.git ~/.tgenv \
+    && echo 'export PATH="$HOME/.tgenv/bin:$PATH"' >> ~/.bashrc
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" \
+    && install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl    
