9a669bccc5e9d15eab0393dea389381bb269189d
Mark Menger <m.menger@f5.com>
2023-03-28 19:14:27 +0000

    basic update to devcontainer.json

--- .devcontainer/scripts/install-dev-tools.sh
+++ .devcontainer/scripts/install-dev-tools.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv \
     && echo 'export PATH=$PATH:$HOME/.tfenv/bin' >> ~/.bashrc
@@ -7,4 +7,4 @@ git clone https://github.com/cunymatthieu/tgenv.git ~/.tgenv \
     && echo 'export PATH="$HOME/.tgenv/bin:$PATH"' >> ~/.bashrc
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" \
-    && install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl    
+    && sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl    
