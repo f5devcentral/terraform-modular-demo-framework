4693715cf9d9a5ebf8618b30034012ab880cd70e
Mark J Menger <mjmenger@users.noreply.github.com>
2023-04-05 01:12:16 +0000

    fix: azure-site-1-app snake case

--- azure-site-1-app/main.tf
+++ azure-site-1-app/main.tf
@@ -1,19 +1,19 @@
 resource "azurerm_kubernetes_cluster" "default" {
-  name                = "${var.projectPrefix}-aks"
-  location            = var.azureRegion
-  resource_group_name = var.resourceGroup
-  dns_prefix          = "${var.projectPrefix}-k8s"
+  name                = "${var.project_prefix}-aks"
+  location            = var.azure_region
+  resource_group_name = var.resource_group
+  dns_prefix          = "${var.project_prefix}-k8s"
 
   default_node_pool {
     name            = "default"
     node_count      = 2
-    vm_size         = var.clusterNodeSize
+    vm_size         = var.cluster_node_size
     os_disk_size_gb = 30
   }
 
   service_principal {
-    client_id     = var.clientID_azurespn
-    client_secret = var.clientID_password
+    client_id     = var.client_id_azurespn
+    client_secret = var.client_id_password
   }
 
   role_based_access_control_enabled = true
@@ -23,36 +23,32 @@ resource "azurerm_kubernetes_cluster" "default" {
   }
 }
 
-variable clientID_azurespn {
+variable client_id_azurespn {
     type = string 
     description = "clientID value for Azure SPN"
 }
 
-variable clientID_password {
+variable client_id_password {
     type = string
     description = "clientID password for Azure SPN"
 }
 
-variable projectPrefix {
+variable project_prefix {
     type = string
     description = "custom project prefix from parent terragrunt.hcl"
 }
-variable azureRegion {
+variable azure_region {
     type = string
     description = "Azure Region for site deployment"
 }
-variable resourceGroup {
+variable resource_group {
     type = string
     description = "Resource group for resources in Azure Site"
 }
 provider "azurerm" {
-  # subscription_id = "${var.subscription_id}"
-  # client_id       = "${var.client_id}"
-  # client_secret   = "${var.client_secret}"
-  # tenant_id       = "${var.tenant_id}"
   features {}
 }
-variable clusterNodeSize{
+variable cluster_node_size{
     type = string
     description = "Size of Nodes to be created for the AKS Node Pool"
     default = "Standard_B2s"
@@ -61,7 +57,7 @@ output kubeconfig {
     value = azurerm_kubernetes_cluster.default.kube_config_raw
     sensitive = true
 }
-variable instanceSuffix {
+variable instance_suffix {
     type = string
     description = "Suffix instance value that co-relates to site and app"
 }
@@ -72,11 +68,11 @@ variable site_name {
     type = string
 }
 
-variable volterraTenant {
+variable volterra_tenant {
     type = string
 }
 resource "volterra_discovery" "cluster-discovery" {
-  name      = format("%s-aksdiscovery-%s",var.projectPrefix,var.instanceSuffix)
+  name      = format("%s-aksdiscovery-%s",var.project_prefix,var.instance_suffix)
   namespace = "system" // Always leave as "system" as Service Discovery objects are created under "System" namespace in XC
 
   // One of the arguments from this list "no_cluster_id cluster_id" must be set
