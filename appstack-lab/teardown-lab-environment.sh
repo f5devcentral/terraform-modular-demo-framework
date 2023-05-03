cb01a7f37eb8b77f608baaa99e7d596d48df7c1e
piyerf5 <p.iyer@f5.com>
2023-05-03 15:25:51 -0400

    teardown-script added

---
+++ appstack-lab/teardown-lab-environment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ -z "$TF_VAR_namespace" ]]; then
+    echo "Must provide TF_VAR_namespace in environment" 1>&2
+    exit 1
+fi
+
+if [[ "$TF_VAR_namespace" == *"<"* ]]; then
+    echo "Please don't paste the example, replace with your namespace" 1>&2
+    exit 1
+fi
+
+cd ~/terraform-modular-demo-framework
+
+terragrunt run-all destroy --terragrunt-modules-that-include ./appstack-lab.hcl --terragrunt-non-interactive
+terragrunt run-all destroy --terragrunt-modules-that-include ./env-setup.hcl --terragrunt-non-interactive
